cb4f2d51294ab5ac1baf0a82dd27e8e03487901a
Himel Ahmed <ahimel963@gmail.com>
2026-05-01 22:29:10 +0600

    Project Init

---
+++ src/app/components/NavLink.jsx
@@ -0,0 +1,23 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+const NavLink = ({ href, children }) => {
+    const pathname = usePathname();
+
+    function isActive() {
+        if (href === pathname) return true;
+        return false;
+    }
+
+    return (
+        <div>
+            <Link href={href} className={` block w-full h-full ${isActive() ? 'border-b-2 border-gray-500' : ''}`}>
+                {children}
+            </Link>
+        </div>
+    );
+};
+
+export default NavLink;
---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,55 @@
+'use client'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import NavLink from './NavLink';
+
+const Navbar = () => {
+
+
+    const link = <>
+        <li><NavLink href="/">Home</NavLink></li>
+        <li><NavLink href="/courses">Courses</NavLink></li> 
+        <li><NavLink href="/profile">My Profile</NavLink></li>
+    </>
+
+    return (
+        <div className='container mx-auto my-3 px-4 md:px-0'>
+            <div className="navbar flex justify-between items-center min-h-16 relative">
+
+                <div className="flex-1 lg:flex-none">
+                    <div className="dropdown lg:hidden">
+                        <div tabIndex={0} role="button" className="btn btn-ghost p-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul tabIndex="-1" className="flex flex-col dropdown-content bg-base-100 rounded-box z-20 mt-3 w-40 p-4 space-y-1 shadow">
+                            {link}
+                        </ul>
+                    </div>
+                    <div className="hidden lg:block w-10"></div>
+                </div>
+
+                <div className="hidden lg:flex absolute left-1/2 -translate-x-1/2">
+                    <ul className="flex items-center gap-8">
+                        {link}
+                    </ul>
+                </div>
+
+                <div className="flex-1 lg:flex-none flex justify-end">
+                    <div className="flex items-center gap-3">
+                        <Link
+                            href="/login"
+                            className="btn btn-sm md:btn-md bg-[#403F3F] hover:bg-[#2b2a2a] text-white rounded-none px-10 font-semibold"
+                        >
+                            Login
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
